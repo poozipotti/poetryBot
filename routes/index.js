1137bd6578f97ac82eb4fda300f83a2a929dd601
George Beckert <georgebeckert@gmail.com>
2023-03-28 15:58:02 -0400

    remove server requirement

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const poemRoutes = require("./poems");
-const path = require("path");
-const constructorMethod = (app) => {
-    app.use("/poems", poemRoutes);
-};
-
-module.exports = constructorMethod;
